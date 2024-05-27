7ad2205fd9d0222e4ed1b8c7348418c52b8918bf
xAlekos <garufialessandro0@gmail.com>
2024-05-27 21:45:19 +0200

    aggiunto draw registers per mostare lo stato dei registri!

--- main.c
+++ main.c
@@ -2,12 +2,27 @@
 #include <raylib.h>
 
 
+
+
+void draw_registers(cpu* ctx){
+    BeginDrawing();
+    DrawText("A:\n\n\n\nX:\n\n\n\nY:\n\n\n\nP:\n\n\n\nS:\n\n\n\n",850,10,40,WHITE);
+    DrawText(TextFormat("%d\n\n\n\n%d\n\n\n\n%d\n\n\n\n$%X\n\n\n\n$%X",ctx->a,ctx->x,ctx->y,ctx->pc,ctx->sp),900,10,40,WHITE);
+    EndDrawing();
+    
+
+
+}
+
 int main(int argc, char** argv){
+    cpu* ctx = cpu_init();
     InitWindow(1024,1024,"6502 emu");
     SetTargetFPS(60);
+    ClearBackground(BLACK);
 
     while(!WindowShouldClose()){
-        ClearBackground(BLACK);
+        draw_registers(ctx);
+        
     }
 }
 
